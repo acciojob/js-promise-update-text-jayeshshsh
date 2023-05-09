51481366d301e28b913de4d7f8c8ef2231ff7cf4
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-05-09 20:09:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const outputElement = document.getElementById("output");
+
+function delayedMessage() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+delayedMessage().then((message) => {
+  outputElement.textContent = message;
+});
